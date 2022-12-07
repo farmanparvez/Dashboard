bb8b814d0b22caf9d68c218716b3001522ea6828
farman <farman@kmindz.in>
2022-12-08 01:34:36 +0530

    fixed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/api', dishRouter)
 
 if (process.env.NODE_ENV === "production") {
     // app.use(express.static("frontend/build"));
-    app.use(express.static(path.resolve(__dirname, "Frontend", "build")));
+    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
   
     app.get("*", (req, res) =>
       res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
